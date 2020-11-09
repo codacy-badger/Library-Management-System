fc8d942fa1555dbd2409b0a010b0e8edf9f8cc48
99002476 <sweta.bharti@ltts.com>
2020-11-09 16:27:20 +0530

    Update library_test.cpp

--- Library/test/library_test.cpp
+++ Library/test/library_test.cpp
@@ -18,6 +18,19 @@ TEST(Book, getNumberOfBooksBasedOnPublisherTest) {
   EXPECT_EQ(3, l1.getNumberOfBooksBasedOnPublisher("Pearson"));
   EXPECT_EQ(0, l1.getNumberOfBooksBasedOnPublisher("Ispring"));
 }
+TEST(Library, countBooksBasedOnGenreTest) {
+  Library l1;
+  EXPECT_EQ(13, l1.countBooksBasedOnGenre("science"));
+  EXPECT_EQ(24, l1.countBooksBasedOnGenre("tech"));
+  EXPECT_EQ(38, l1.countBooksBasedOnGenre("fiction"));
+  EXPECT_EQ(36, l1.countBooksBasedOnGenre("nonfiction"));
+  EXPECT_EQ(9, l1.countBooksBasedOnGenre("philosophy"));
+  EXPECT_EQ(0, l1.countBooksBasedOnGenre("nonscience"));
+}
+TEST(Book, getBookWithMaxPagesTest) {
+  Book book;
+  EXPECT_EQ("Computer Vision A Modern Approach", book.getBookWithMaxPages());
+}
 
 
 } // namespace
