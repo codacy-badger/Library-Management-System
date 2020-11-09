587d543423f4475b204d88b027106a88c6d41ea8
99002507 <umamaheswarreddy.nagirireddi@ltts.com>
2020-11-09 11:09:05 +0530

    Create book.h

---
+++ Library/inc/book.h
@@ -0,0 +1,30 @@
+
+#ifndef __BOOK_
+#define __BOOK_
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+class Book {
+int m_id;
+  std::string m_title;
+  std::string m_author1;
+  std::string m_author2;
+  std::string m_genre;
+  std::string m_subGenre;
+  int m_pages;
+  std::string m_publisher;
+
+public:
+  std::string addBook(int, std::string, std::string, std::string, std::string,
+                      std::string, int, std::string);
+  std::string deleteBook(int);
+  std::string getBookNameBasedOnID(int);
+  std::string getBookWithMaxPages();
+  std::string getBookWithMinPages();
+};
+
+#endif
