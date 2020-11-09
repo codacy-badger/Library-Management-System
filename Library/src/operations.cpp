af0ab2ba00be0992c36a11ddad528b7959152824
99002665 <sailakshmikvr1998@gmail.com>
2020-11-09 15:32:00 +0530

    create operations.cpp

---
+++ Library/src/operations.cpp
@@ -0,0 +1,28 @@
+#include "operations.h"
+#include "book.cpp"
+
+int Library::getNumberOfBooksBasedOnPublisher(std::string publisher) {
+
+  fstream fin;
+  fin.open("books_new.csv");
+  string publisher2;
+  count = 0;
+  vector<string> row;
+  string line, word, temp;
+  getline(fin, line);
+
+  while (getline(fin, line)) {
+    row.clear();
+    stringstream s(line);
+    while (getline(s, word, ',')) {
+      row.push_back(word);
+    }
+    publisher2 = row[7];
+    if (publisher.compare(publisher2) == 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+s
--- Library/test/library_test.cpp
+++ Library/test/library_test.cpp
@@ -3,20 +3,6 @@
 #include <gtest/gtest.h>
 namespace {
 
-TEST(Book, getBookNameBasedOnIDTest) {
-  Book book;
-  EXPECT_EQ("Age of Wrath", book.getBookNameBasedOnID(15));
-  EXPECT_EQ("Doctor in the Nude", book.getBookNameBasedOnID(100));
-}
-TEST(Book, getBookWithMinPagesTest) {
-  Book book;
-  EXPECT_EQ("Ashenden of The British Agent", book.getBookWithMinPages());
-}
-TEST(Book, getBookWithMaxPagesTest) {
-  Book book;
-  EXPECT_EQ("Computer Vision A Modern Approach", book.getBookWithMaxPages());
-}
-
 
 
 } // namespace
