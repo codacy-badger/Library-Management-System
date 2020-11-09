8f05dede62c77e1af81d6463ea048fc3da30c93c
99002665 <sailakshmikvr1998@gmail.com>
2020-11-09 12:43:36 +0530

    Update book.cpp

--- Library/src/book.cpp
+++ Library/src/book.cpp
@@ -13,3 +13,35 @@ std::string Book::addBook(int id, std::string title, std::string author1,
   return "Book added successfully";
 }
 
+
+std::string Book::deleteBook(int id) { 
+fstream fin;
+    fstream fout;
+    fin.open("books_new.csv", ios::in);
+    fout.open("new.csv",ios::out | ios::app);
+
+    vector<string> row;
+  string line, word, temp;
+  getline(fin, line);
+
+  while (getline(fin, line)) {
+    row.clear();
+    stringstream s(line);
+    while (getline(s, word, ',')) {
+      row.push_back(word);
+      
+    }
+    if(id != stoi(row[0])){
+    fout << line << endl;
+
+    }
+    }
+  
+    fout.close();
+    fin.close();
+
+    remove("books_new.csv");
+    rename("new.csv", "books_new.csv");
+    
+return "Book removed successfully"; }
+
