08f6b1b904b4575a1bee8b1e5e010e7a6636c5d5
99002507 <umamaheswarreddy.nagirireddi@ltts.com>
2020-11-09 14:15:52 +0530

    Update library_test.cpp

--- Library/test/library_test.cpp
+++ Library/test/library_test.cpp
@@ -8,6 +8,10 @@ TEST(Book, getBookNameBasedOnIDTest) {
   EXPECT_EQ("Age of Wrath", book.getBookNameBasedOnID(15));
   EXPECT_EQ("Doctor in the Nude", book.getBookNameBasedOnID(100));
 }
+TEST(Book, getBookWithMinPagesTest) {
+  Book book;
+  EXPECT_EQ("Ashenden of The British Agent", book.getBookWithMinPages());
+}
 
 
 } // namespace
